7586774d53052c800ff7a89b215efed0a1656577
andrebzr <andresantosbzr01@gmail.com>
2026-07-28 18:42:26 -0300

    Adiciona ordenaçao de produtos por preço

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Collections;
+import java.util.Comparator;
+
 
 public class Main {
 
@@ -15,6 +18,8 @@ public class Main {
 	    System.out.println("2 - Listar produtos");
 	    System.out.println("3 - Atualizar produto");
 	    System.out.println("4 - Deletar produto");
+	    System.out.println("5 - Ordenar por preco (crescente)");
+	    System.out.println("6 - Ordernar por preco (decrescente)");
 	    System.out.println("0 - Sair");
 	    System.out.print("Escolha uma opção: ");
 	    
@@ -22,8 +27,10 @@ public class Main {
 	    	opcao = scanner.nextInt();
 	    } catch (Exception e) {
 	    	System.out.println("Erro: digite somente numeros!");
-	    	
+	    	scanner.nextLine();
+	    	continue;	    	
 	    }
+	    
 
 	    switch (opcao) {
 	        case 1:
@@ -37,7 +44,15 @@ public class Main {
 	            }
 	           
 	            System.out.println("Digite o preco do produto: ");
-	            double preco = scanner.nextDouble();
+	            double preco = 0;
+	            try {
+	            	preco = scanner.nextDouble();
+	            } catch (Exception e) {
+	            	System.out.println("Erro: digite um valor numerico valido para o preco.");
+	            	scanner.nextLine();
+	            	break;
+	            }
+	            
 	            
 	            if (preco < 0) {
 	            	System.out.println("Erro: Preco nao pode ser negativo!");
@@ -110,6 +125,13 @@ public class Main {
 	        		System.out.println("Produto deletado com sucesso.");
 	        	}
 	        	break;
+	        case 5:
+	        	produtos.sort(Comparator.comparingDouble(Produto::getPreco));
+	        	System.out.println("Produtos ordenados por preco (crescente)!");
+	        	break;
+	        case 6:
+	        	produtos.sort(Comparator.comparingDouble(Produto::getPreco).reversed());
+	        	System.out.println("Produtos ordenados por preco (decrescente)!");
 	        case 0:
 	            System.out.println("Saindo...");
 	            break;
